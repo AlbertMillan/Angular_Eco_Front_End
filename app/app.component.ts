03be936ed8655b5518f5888819bf4591130becc4
Albert Millan <millana@tcd.ie>
2018-09-08 13:48:04 +0200

    Finished full front-page layout
    Finished full front-page layout

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { AuthService } from './../providers/authService/auth.service';
+//import 'jquery.scrollto';
+
+declare var $:any;
+
 
 @Component({
   selector: 'app-root',
@@ -11,6 +15,10 @@ export class AppComponent {
   productDetail:any;
   currentProduct:any;
   currentProductDetail: any;
+  email: any;
+  isValidEmail:boolean = true;
+  hasValidatedEmail: boolean = false;
+  
 
   title = 'My first App!';
 
@@ -39,7 +47,7 @@ export class AppComponent {
     this.authService.getProducts().subscribe((res) => {
       console.log(res);
       this.productList = res;
-    })
+    }); 
   }
 
   registerUser() {
@@ -54,14 +62,30 @@ export class AppComponent {
     this.authService.getProductDetails( 'specs/', product.id ).subscribe( res => {
       console.log(res);
       this.productDetail = res;
+      this.currentProductDetail = res[0];
     });
 
     this.currentProduct = product;
+    
+    $(window).scrollTo({left:0, top:520}, {duration:1500});
   }
 
   getCurrentProduct(item){
     console.log(item);
     this.currentProductDetail = item;
+  }
+
+  subscribeNewsletter() {
+    var regex = new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/);
+    if( regex.test(this.email) ){
+      console.log("Valid Email");
+      this.isValidEmail = true;
+      this.hasValidatedEmail = true;
+    }
+    else {
+      console.log("Invalid Email");
+      this.isValidEmail = false;
+    }
   }
 
   showAll() {
@@ -76,32 +100,39 @@ export class AppComponent {
   showTa() {
     this.option = 3;
   }
+
+  inputContainer(){
+    let styles = {
+      // 'border-color': this.isValidEmail ? 'rgba(0,0,0,.15)':'#c42e2e',
+      'display': this.hasValidatedEmail ? 'none':'flex',
+    };
+    return styles;
+  }
+
+  inputElement(){
+    let styles = {
+      'border-color': this.isValidEmail ? 'rgba(0,0,0,.15)':'#c42e2e',
+    };
+    return styles;
+  }
+
+  errorMessage() {
+    let styles = {
+      'visibility': this.isValidEmail ? 'hidden':'visible',
+      'opacity': this.isValidEmail ? '0':'1',
+    };
+    return styles;
+  }
+
+  successMessage() {
+    let styles = {
+      'visibility': this.hasValidatedEmail ? 'visible':'hidden',
+      'opacity': this.hasValidatedEmail ? '1':'0',
+    };
+    return styles;
+  }
+
+
 }
 
-/*import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent implements OnInit {
-  title = 'My first App!';
-  people:any[]
-  //person = {"name":"", "age":""};
-  searchbox:string;
-
-  ngOnInit(){
-    this.title = 'My first App!';
-    this.people = [
-      {
-        "name": "Maria",
-        "age": 18
-      },
-      {
-        "name": "Lucian",
-        "age": 20
-      }
-    ];
-  }
-}*/
--- app/app.module.ts
+++ app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';     // Allows *ngFor
 import { HttpClientModule } from '@angular/common/http';
+//import { ScrollToModule } from 'ng2-scroll-to-el';
 
 import { AppComponent } from './app.component';
 import { FilterPipe } from './pipes/filter.pipe';
@@ -17,6 +18,7 @@ import { AuthService } from './../providers/authService/auth.service';
     BrowserModule,
     CommonModule,
     FormsModule,
+   // ScrollToModule.forRoot(),
     HttpClientModule
   ],
   providers: [AuthService],
